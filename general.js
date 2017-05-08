53fe9c4d7c275dff22d7e1ed2678877db9be5bd5
linseyo <reedlinsey@gmail.com>
2017-05-07 21:21:12 -0700

    Add details to each part of my workflow

--- general.js
+++ general.js
@@ -3,13 +3,35 @@ $( document ).ready(function() {
   $('.iterate').find('p').hide();
   $('.deliver').find('p').hide();
 
-  $('.plan').on("click", "button", function(e){
+  $('.plan').on("click", "img", function(e){
     e.preventDefault();
+    $('.plan').find('p').hide();
+    $('.iterate').find('p').hide();
+    $('.deliver').find('p').hide();
     console.log("made it again!")
     $('.plan').find('p').slideDown();
 
   })
 
+  $('.iterate').on("click", "img", function(e){
+    e.preventDefault();
+    $('.plan').find('p').hide();
+    $('.iterate').find('p').hide();
+    $('.deliver').find('p').hide();
+    console.log("made it again!")
+    $('.iterate').find('p').slideDown();
+
+  })
+
+  $('.deliver').on("click", "img", function(e){
+    e.preventDefault();
+    $('.plan').find('p').hide();
+    $('.iterate').find('p').hide();
+    $('.deliver').find('p').hide();
+    console.log("made it again!")
+    $('.deliver').find('p').slideDown();
+
+  })
 
 
 
