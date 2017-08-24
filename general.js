bd0723cf11527f385f252a4a62c0c2c960bb8832
linseyo <reedlinsey@gmail.com>
2017-08-23 22:45:59 -0700

    Add new gif and type plugin

--- general.js
+++ general.js
@@ -1,66 +1,68 @@
 $( document ).ready(function() {
-  $(function () {
-    var $element = $('#notice');
-    setInterval(function () {
-        $element.fadeIn(1000, function () {
-            $element.fadeOut(1500, function () {
-                $element.fadeIn(1500)
-            });
-        });
-    }, 1000);
-  });
 
-  $('.plan').find('p').hide();
-  $('.iterate').find('p').hide();
-  $('.deliver').find('p').hide();
-  $('.design-code-content').hide();
-  $('.photo-video-content').hide();
-  $('.tech-skill-content').hide();
-  $('.ot-skill-content').hide();
-  $('.exp-skill-content').hide();
 
-  $('.plan').on("click", "img", function(e){
-    e.preventDefault();
-    $('.plan').find('p').slideToggle();
-
-  })
-
-  $('.iterate').on("click", "img", function(e){
-    e.preventDefault();
-    $('.iterate').find('p').slideToggle();
-
-  })
-
-  $('.deliver').on("click", "img", function(e){
-    e.preventDefault();
-    $('.deliver').find('p').slideToggle();
-
-  })
-
-  $('.design-code').on("click", "h1", function(e){
-    e.preventDefault();
-    $('.design-code-content').slideToggle();
-  })
-
-  $('.photo-video').on("click", "h1", function(e){
-    e.preventDefault();
-    $('.photo-video-content').slideToggle();
-  })
-
-  $('.technical-skill').on("click", "h3", function(e){
-    e.preventDefault();
-    $('.tech-skill-content').slideToggle();
-  })
-
-  $('.other-skill').on("click", "h3", function(e){
-    e.preventDefault();
-    $('.ot-skill-content').slideToggle();
-  })
-
-  $('.explore-skill').on("click", "h3", function(e){
-    e.preventDefault();
-    $('.exp-skill-content').slideToggle();
-  })
+  // $(function () {
+  //   var $element = $('#notice');
+  //   setInterval(function () {
+  //       $element.fadeIn(1000, function () {
+  //           $element.fadeOut(1500, function () {
+  //               $element.fadeIn(1500)
+  //           });
+  //       });
+  //   }, 1000);
+  // });
+  //
+  // $('.plan').find('p').hide();
+  // $('.iterate').find('p').hide();
+  // $('.deliver').find('p').hide();
+  // $('.design-code-content').hide();
+  // $('.photo-video-content').hide();
+  // $('.tech-skill-content').hide();
+  // $('.ot-skill-content').hide();
+  // $('.exp-skill-content').hide();
+  //
+  // $('.plan').on("click", "img", function(e){
+  //   e.preventDefault();
+  //   $('.plan').find('p').slideToggle();
+  //
+  // })
+  //
+  // $('.iterate').on("click", "img", function(e){
+  //   e.preventDefault();
+  //   $('.iterate').find('p').slideToggle();
+  //
+  // })
+  //
+  // $('.deliver').on("click", "img", function(e){
+  //   e.preventDefault();
+  //   $('.deliver').find('p').slideToggle();
+  //
+  // })
+  //
+  // $('.design-code').on("click", "h1", function(e){
+  //   e.preventDefault();
+  //   $('.design-code-content').slideToggle();
+  // })
+  //
+  // $('.photo-video').on("click", "h1", function(e){
+  //   e.preventDefault();
+  //   $('.photo-video-content').slideToggle();
+  // })
+  //
+  // $('.technical-skill').on("click", "h3", function(e){
+  //   e.preventDefault();
+  //   $('.tech-skill-content').slideToggle();
+  // })
+  //
+  // $('.other-skill').on("click", "h3", function(e){
+  //   e.preventDefault();
+  //   $('.ot-skill-content').slideToggle();
+  // })
+  //
+  // $('.explore-skill').on("click", "h3", function(e){
+  //   e.preventDefault();
+  //   $('.exp-skill-content').slideToggle();
+  // })
 
 
 });
